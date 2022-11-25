cedf614205876a7699902204579ac043fe2c3cbe
OgabekDev <ogabekdev@gmail.com>
2022-11-25 13:49:56 +0500

    Initial Commit

---
+++ MyStart/MyStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStartApp.swift
+//  MyStart
+//
+//  Created by Ogabek Matyakubov on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MyStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
