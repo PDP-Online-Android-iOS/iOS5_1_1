721620cc904b1e4abb34672186f3f0a2cab43140
OgabekDev <ogabekdev@gmail.com>
2022-11-25 14:02:11 +0500

    All Done

---
+++ MyStart/screens/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  MyStart
+//
+//  Created by Ogabek Matyakubov on 25/11/22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text("My Start")
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
